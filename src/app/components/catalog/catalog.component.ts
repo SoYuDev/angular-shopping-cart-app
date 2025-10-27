11d303e28862e1209e01ff78e91629c21feadd05
Luis Rodríguez <luisrodriguezcamas99@gmail.com>
2025-10-27 18:47:17 +0100

    feat: add sub-components

---
+++ src/app/components/catalog/catalog.component.ts
@@ -0,0 +1,16 @@
+import { Component, Input } from '@angular/core';
+import { Product } from '../../model/product';
+import { ProductCardComponent } from '../product-card/product-card.component';
+
+@Component({
+  selector: 'app-catalog',
+  standalone: true,
+  imports: [ProductCardComponent],
+  templateUrl: './catalog.component.html',
+  styleUrl: './catalog.component.css'
+})
+export class CatalogComponent {
+
+  @Input() products!: Product[];
+
+}
