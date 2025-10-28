0968553d38223879feef395f23f374fa1b8789cb
Luis Rodríguez <luisrodriguezcamas99@gmail.com>
2025-10-28 19:34:13 +0100

    feat: add delete button

--- src/app/components/cart-app/cart-app.component.ts
+++ src/app/components/cart-app/cart-app.component.ts
@@ -44,7 +44,7 @@ export class CartAppComponent implements OnInit {
       this.items = this.items.map((item) => {
         if (item.product.id === product.id) {
           return {
-            ... item,
+            ...item,
             quantity: item.quantity + 1,
           };
         }
@@ -53,5 +53,12 @@ export class CartAppComponent implements OnInit {
     } else {
       this.items = [...this.items, { product: { ...product }, quantity: 1 }];
     }
+  }
+
+  onDeleteCart(id: number): void {
+    //Filtra todo aquello que cumpla la condición.
+    this.items = this.items.filter( item => {
+      item.product.id !== id;
+    })
   }
 }
--- src/app/components/cart/cart.component.ts
+++ src/app/components/cart/cart.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { CartItem } from '../../model/cartItem';
 
 @Component({
@@ -11,5 +11,11 @@ import { CartItem } from '../../model/cartItem';
 export class CartComponent {
 
   @Input() items: CartItem[] = [];
+
+  @Output() idProductEventEmitter = new EventEmitter()
+
+  onDeleteCart(id: number) {
+    this.idProductEventEmitter.emit(id);
+  }
 
 }
