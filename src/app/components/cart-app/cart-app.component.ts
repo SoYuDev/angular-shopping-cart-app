dc0dbafb7f0906dc7f3bf4f2c5612ea07a440291
Luis Rodríguez <luisrodriguezcamas99@gmail.com>
2025-10-28 19:25:13 +0100

    feat: fixed quantity problem

--- src/app/components/cart-app/cart-app.component.ts
+++ src/app/components/cart-app/cart-app.component.ts
@@ -27,10 +27,31 @@ export class CartAppComponent implements OnInit {
 
   // Añadimos el producto a partir de los eventos de los componentes hijos product-card -> catalog -> cart-app
   onAddCart(product: Product): void {
-    
+    // Comprobamos si el producto que estamos añadiendo existe ya en nuestro atributo items.
+    // El valor de hasItem será o de tipo CartItem si se cumple la condición o undefined
+    const hasItem = this.items.find((item) => {
+      return item.product.id === product.id;
+    });
+
+    // Manera simplificada de poner la función callback.
+    // const hasItem = this.items.find(item => item.product.id === product.id);
+
     // ... this.items hace una copia de la instancia y además le añadimos el producto que nos viene por el argumento y la cantidad, que será 1 por defecto.
     // { product: {... product}, lo hacemos para pasar una nueva instancia a partir del product. Esto lo hacemos para que si cambiamos el producto
     // Del carro este no afecte al producto del catálogo. Simplemente es una decisión de diseño.
-    this.items = [...this.items, { product: {... product}, quantity: 1 }];
+    if (hasItem) {
+      // .map devuelve una nueva instancia de los items pero modificando el item, en este caso modificamos la cantidad.
+      this.items = this.items.map((item) => {
+        if (item.product.id === product.id) {
+          return {
+            ... item,
+            quantity: item.quantity + 1,
+          };
+        }
+        return item;
+      });
+    } else {
+      this.items = [...this.items, { product: { ...product }, quantity: 1 }];
+    }
   }
 }
